f4eff1385b14f3a47f552a73ae49e04a52e16116
initial commit <Behzad Shakeri@BEHZAD>
2019-02-07 13:23:07 +0330

    ok

--- MyApplication/LoginForm.cs
+++ MyApplication/LoginForm.cs
@@ -13,20 +13,27 @@ namespace MyApplication
 
         private void loginButton_Click(object sender, System.EventArgs e)
         {
-            //******************************************************
-            //******************************************************
-            //******************************************************
-            if ((string.IsNullOrWhiteSpace(usernameTextBox.Text)) ||
-                (string.IsNullOrWhiteSpace(passwordTextBox.Text)))
+            // **************************************************
+            // **************************************************
+            // **************************************************
+            if (string.IsNullOrWhiteSpace(usernameTextBox.Text) ||
+                string.IsNullOrWhiteSpace(passwordTextBox.Text))
             {
-                //usernameTextBox.Text = usernameTextBox.Text.Trim();
-                //passwordTextBox.Text = passwordTextBox.Text.Trim();
+                //usernameTextBox.Text =
+                //	usernameTextBox.Text.Trim();
 
-                usernameTextBox.Text = usernameTextBox.Text.Replace(" ", string.Empty);
-                passwordTextBox.Text = passwordTextBox.Text.Replace(" ", string.Empty);
+                //passwordTextBox.Text =
+                //	passwordTextBox.Text.Trim();
 
-                System.Windows.Forms.MessageBox.Show("Username And Password Is Required");
-                if (usernameTextBox.Text==string.Empty)
+                usernameTextBox.Text =
+                    usernameTextBox.Text.Replace(" ", string.Empty);
+
+                passwordTextBox.Text =
+                    passwordTextBox.Text.Replace(" ", string.Empty);
+
+                System.Windows.Forms.MessageBox.Show("Username and Password is requied!");
+
+                if (usernameTextBox.Text == string.Empty)
                 {
                     usernameTextBox.Focus();
                 }
@@ -35,11 +42,133 @@ namespace MyApplication
                     passwordTextBox.Focus();
                 }
 
-                return ;
+                return;
             }
-            //******************************************************
-            //******************************************************
-            //******************************************************
+            // **************************************************
+            // **************************************************
+            // **************************************************
+
+            // از این قسمت به بعد باید سر کلاس نوشته شود
+
+            Models.DatabaseContext databaseContext = null;
+
+            try
+            {
+                databaseContext =
+                    new Models.DatabaseContext();
+
+                Models.User foundedUser =
+                    databaseContext.Users
+                    .Where(current => string.Compare(current.Username, usernameTextBox.Text, true) == 0)
+                    .FirstOrDefault();
+
+                if (foundedUser == null)
+                {
+                    //System.Windows.Forms.MessageBox
+                    //	.Show("Username is not correct!");
+
+                    // دقت کنید که در این حالت پیغام باید گنگ باشد
+
+                    System.Windows.Forms.MessageBox
+                        .Show("Username and/or Password is not correct!");
+
+                    usernameTextBox.Focus();
+
+                    return;
+                }
+
+                if (string.Compare(foundedUser.Password, passwordTextBox.Text, ignoreCase: false) != 0)
+                {
+                    //System.Windows.Forms.MessageBox
+                    //	.Show("Password is not correct!");
+
+                    // دقت کنید که در این حالت پیغام باید گنگ باشد
+
+                    System.Windows.Forms.MessageBox
+                        .Show("Username and/or Password is not correct!");
+
+                    usernameTextBox.Focus();
+
+                    return;
+                }
+
+                if (foundedUser.IsActive == false)
+                {
+                    System.Windows.Forms.MessageBox
+                        .Show("You can not login to this application! Please contact support team.");
+
+                    usernameTextBox.Focus();
+
+                    return;
+                }
+
+                // **************************************************
+                // **************************************************
+                // **************************************************
+                System.Windows.Forms.MessageBox.Show("Welcome!");
+                // **************************************************
+
+                // **************************************************
+                //Infrastructure.Utility.AuthenticatedUser = foundedUser;
+
+                //Hide();
+                // **************************************************
+
+                // **************************************************
+                //MainForm mainForm = new MainForm();
+
+                //mainForm.InitializeMainForm();
+
+                //mainForm.Show();
+                // **************************************************
+
+                // **************************************************
+                //Infrastructure.Utility.MainForm.InitializeMainForm();
+
+                //Infrastructure.Utility.MainForm.Show();
+                // **************************************************
+                // **************************************************
+                // **************************************************
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                if (databaseContext != null)
+                {
+                    databaseContext.Dispose();
+                    databaseContext = null;
+                }
+            }
+            ////******************************************************
+            ////******************************************************
+            ////******************************************************
+            //if ((string.IsNullOrWhiteSpace(usernameTextBox.Text)) ||
+            //    (string.IsNullOrWhiteSpace(passwordTextBox.Text)))
+            //{
+            //    //usernameTextBox.Text = usernameTextBox.Text.Trim();
+            //    //passwordTextBox.Text = passwordTextBox.Text.Trim();
+
+            //    usernameTextBox.Text = usernameTextBox.Text.Replace(" ", string.Empty);
+            //    passwordTextBox.Text = passwordTextBox.Text.Replace(" ", string.Empty);
+
+            //    System.Windows.Forms.MessageBox.Show("Username And Password Is Required");
+            //    if (usernameTextBox.Text==string.Empty)
+            //    {
+            //        usernameTextBox.Focus();
+            //    }
+            //    else
+            //    {
+            //        passwordTextBox.Focus();
+            //    }
+
+            //    return ;
+            //}
+            ////******************************************************
+            ////******************************************************
+            ////******************************************************
         }
 
         private void registerButton_Click(object sender, System.EventArgs e)
