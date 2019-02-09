509cfb5dd914eb93edbe81250fa99b68fb980d69
initial commit <Behzad Shakeri@BEHZAD>
2019-02-09 11:55:57 +0330

    Add Comments-1

--- MyApplication/Models/User.cs
+++ MyApplication/Models/User.cs
@@ -7,20 +7,26 @@
 		}
 
 		// **********
+        //برای ساخت فرم لاگین ، رجیستر دارد و ... لزم است
 		public bool IsActive { get; set; }
 		// **********
 
 		// **********
+        //اضافه کردن برای اختصاص یک منوی اضافه برای ادمین، مثل منوی کاربران و ...
 		public bool IsAdmin { get; set; }
-		// **********
+        // **********
 
-		// **********
-		[System.ComponentModel.DataAnnotations.Required
+        // **********
+        //Attribiuts
+        // این اتریبیوتها برای نازک کاری انتیتی فریم ورک هستند
+        //هم در اپلیکیشن موثر است و هم در دیتابیس
+        //با این دستور نباید مقدار آن nullstring  شود
+        [System.ComponentModel.DataAnnotations.Required
 			(AllowEmptyStrings = false)]
-
+        //برای تعیین حداقل و حداکثر طول فیلد
 		[System.ComponentModel.DataAnnotations.StringLength
 			(maximumLength: 20, MinimumLength = 6)]
-
+        //این فقط در دیتابیس موثر است
 		[System.ComponentModel.DataAnnotations.Schema.Index
 			(IsUnique = true)]
 		public string Username { get; set; }
